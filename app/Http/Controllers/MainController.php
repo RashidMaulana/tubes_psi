4d2370f11878dfc03708cc66da93db91d3fc023d
Rashid <19523030@students.uii.ac.id>
2021-06-23 19:22:57 +0700

    tambahan halaman profil

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 use App\Models\Pemilik;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     function profile() {
-        $data = ['LoggedUserInfo'=>Pemilik::where('id','=',session('LoggedUser'))->first()];
-        return view('profile',$data);
+        $data = Pemilik::where('id','=',session('LoggedUser'))->first();
+        $destinasi = DB::table('destinations')->where('pemiliks_id','=',$data->id)->get();
+        return view('profile',['LoggedUserInfo'=>$data, 'destinasi'=>$destinasi]);
     }
 }
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.template')
 @section('title','Profil')
+@php
+    $number=1;
+@endphp
 @section('content')
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -22,29 +25,59 @@
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-3">
-
+        <div class="row"> <!-- /.row -->
+          <div class="col-md-3"> <!-- /.col -->
             <!-- Profile Image -->
-            <div class="card card-primary card-outline">
+            <div class="card card-primary card-outline text-center">
               <div class="card-body box-profile">
                 <div class="text-center">
                   <img class="profile-user-img img-fluid img-circle"
                        src="{{ asset('') }}assets/dist/img/user2-160x160.jpg"
                        alt="User profile picture">
                 </div>
-
                 <h3 class="profile-username text-center">{{ $LoggedUserInfo ['nama']}}</h3>
-
-                <p class="text-muted text-center">Admin</p>
-
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
+                <ul class="list-group list-group-unbordered mb-3">
+                    <p class="text-muted text-center">Admin</p>
+                  <li class="list-group-item">
+                    <p class="card-text">{{$LoggedUserInfo['alamat']}}</p>
+                  </li>
+                  <li class="list-group-item">
+                    <p class="card-text">{{$LoggedUserInfo['email']}}</p>
+                  </li>
+                </ul>
+              </div> <!-- /.card-body -->
+            </div> <!-- /.card -->
+          </div><!-- /.row -->
+      <div class="col-md-9">
+        <div class="card">
+          <div class="card-header p-2">
+            <h2 class="text-center">Daftar Destinasi</h1><hr>
           </div>
-          <!-- /.col -->
-        <!-- /.row -->
+          <div class="table-responsive">
+            <table class="table table-striped table-hover table-condensed">
+              <thead>
+                
+                <tr>
+                  <th><strong>No</strong></th>
+                  <th><strong>Nama Destinasi</strong></th>
+                  <th><strong>Telephone</strong></th>
+                  <th><strong>Alamat</strong></th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($destinasi as $d)
+                <tr>
+                  <td>{{$number++}}</td>
+                  <th>{{$d->nama}}</th>
+                  <th>{{$d->telephone}}</th>
+                  <th>{{$d->alamat}}</th>
+                  @endforeach
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
