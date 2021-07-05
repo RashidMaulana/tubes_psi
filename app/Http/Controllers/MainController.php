ff7e519ca6da0dabcdee700e2df2666dd68ea89b
Rashid <19523030@students.uii.ac.id>
2021-07-05 20:46:10 +0700

    Membenarkan halaman grafik keuntungan

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use App\Models\Pemilik;
+use App\Models\Transactions;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -26,11 +27,15 @@ class MainController extends Controller
     function grafikpengunjung(){
         $data = Pemilik::where('id','=',session('LoggedUser'))->first();
 
+        $tanggal = Transactions::select('tanggal')->join('facilities as f','transactions.facilities_id','f.id')
+                                              ->join('destinations as d','f.destinations_id','d.id')
+                                              ->where('d.pemiliks_id',$data->id)->groupBy('tanggal')->orderBy('tanggal','ASC')->get()->toArray();
         $visitor = Transactions::select(DB::raw('sum(visitors_id) as visitor'))
         ->join('facilities as f','transactions.facilities_id','f.id')
         ->join('destinations as d','f.destinations_id','d.id')
-        ->where('d.pemiliks_id',$data->id)->groupBy('bulan')->get()->toArray();
-        return view('grafik',['LoggedUserInfo' => $data, 'visitor' => $visitor]);
+        ->where('d.pemiliks_id',$data->id)->groupBy('tanggal')->get()->toArray();
+
+        return view('grafik',['LoggedUserInfo' => $data,'tanggal' => json_encode($tanggal, JSON_NUMERIC_CHECK), 'visitor' => json_encode($visitor, JSON_NUMERIC_CHECK)]);
 
     }
 }
--- resources/views/grafik.blade.php
+++ resources/views/grafik.blade.php
@@ -7,31 +7,24 @@
 <script src="{{ asset('assets/dist/js/pages/dashboard3.js') }}"></script>
 
 <script>
+  var get_tanggal = {!!$tanggal!!}
+  var tanggal = get_tanggal.map(tanggal => tanggal.tanggal);
+  var get_visitor = {!!$visitor!!}
+  var visitor = get_visitor.map(visitor => visitor.visitor);
 var areaChartData = {
-      labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels  : tanggal,
       datasets: [
         {
-          label               : 'Digital Goods',
+          label               : 'Jumlah Total Pengunjung Perhari',
           backgroundColor     : 'rgba(60,141,188,0.9)',
           borderColor         : 'rgba(60,141,188,0.8)',
-          pointRadius          : false,
+          pointRadius          : true,
           pointColor          : '#3b8bba',
           pointStrokeColor    : 'rgba(60,141,188,1)',
           pointHighlightFill  : '#fff',
           pointHighlightStroke: 'rgba(60,141,188,1)',
-          data                : [28, 48, 40, 19, 86, 27, 90]
-        },
-        {
-          label               : 'Electronics',
-          backgroundColor     : 'rgba(210, 214, 222, 1)',
-          borderColor         : 'rgba(210, 214, 222, 1)',
-          pointRadius         : false,
-          pointColor          : 'rgba(210, 214, 222, 1)',
-          pointStrokeColor    : '#c1c7d1',
-          pointHighlightFill  : '#fff',
-          pointHighlightStroke: 'rgba(220,220,220,1)',
-          data                : [65, 59, 80, 81, 56, 55, 40]
-        },
+          data                : visitor
+        }
       ]
     }
 
@@ -44,7 +37,7 @@ var areaChartData = {
       scales: {
         xAxes: [{
           gridLines : {
-            display : false,
+            display : true,
           }
         }],
         yAxes: [{
@@ -54,14 +47,11 @@ var areaChartData = {
         }]
       }
     }
-
-
     var lineChartCanvas = $('#grafik').get(0).getContext('2d')
     var lineChartOptions = $.extend(true, {}, areaChartOptions)
     var lineChartData = $.extend(true, {}, areaChartData)
     lineChartData.datasets[0].fill = false;
-    lineChartData.datasets[1].fill = false;
-    lineChartOptions.datasetFill = false
+    lineChartOptions.datasetFill = false;
 
     var lineChart = new Chart(lineChartCanvas, {
       type: 'line',
@@ -77,16 +67,7 @@ var areaChartData = {
       <div class="container-fluid">
         <div class="card card-info">
               <div class="card-header">
-                <h3 class="card-title">Januari</h3>
-
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn btn-tool" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                  </button>
-                </div>
+                <h3 class="card-title">Grafik Harian Jumlah Pengunjung</h3>
               </div>
               <div class="card-body">
                 <div class="chart">
